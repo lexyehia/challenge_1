9b24bdd3c9bf4c41eeceeab3aaa57dda34086313
Lex Yehia <lex.yehia@icloud.com>
2017-11-21 19:48:03 -0500

    Added basic Workflows Page, viz basic save and reset buttons;

---
+++ src/pages/WorkflowsPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { NavBar } from '../components/NavBar';
+
+export class WorkflowsPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <NavBar />
+                <div style={styles.container}>
+                </div>
+                <div>
+                    <Button
+                        style={{ margin: 10 }}
+                        bsStyle="success"
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        style={{ margin: 10 }}
+
+                        bsStyle="warning"
+                    >
+                        Reset
+                    </Button>
+                </div>
+            </div>
+        )
+    }
+}
