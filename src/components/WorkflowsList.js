899bdace0d9b80a6aa066a56a3a638c1216028aa
Lex Yehia <lex.yehia@icloud.com>
2017-11-21 20:37:55 -0500

    Adding some styling to workflow list, added list of workflows to state (hard-coded for now)

--- src/components/WorkflowsList.js
+++ src/components/WorkflowsList.js
@@ -6,24 +6,28 @@ const styles = {
     container: {
         display: 'flex',
         direction: 'row',
+        margin: '0 auto',
+        justifyContent: 'space-between',
         width: 500,
-    }
-}
+    },
+};
 
 
 export const WorkflowsList = (props) => (
-    <div style={styles.container}>
-        {props.workflows.map(workflow => (
-            <WorkflowPanel
-                key={workflow.id}
-                id={workflow.id}
-                action={workflow.action}
-            />
-        ))}
+    <div>
+        <h4>Current sequence of workflows:</h4><br />
+        <div style={styles.container}>
+            {props.workflows.map(workflow => (
+                <WorkflowPanel
+                    key={workflow.id}
+                    id={workflow.id}
+                    text={workflow.action}
+                />
+            ))}
+        </div>
     </div>
 );
 
 WorkflowsList.propTypes = {
     workflows: PropTypes.array.isRequired,
 };
-
--- src/pages/WorkflowsPage.js
+++ src/pages/WorkflowsPage.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { NavBar } from '../components/NavBar';
+import { WorkflowsList } from '../components/WorkflowsList';
 
 export class WorkflowsPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            workflows: [
+                {
+                    "id": 1,
+                    "action": "Import",
+                    "isStart": true,
+                    "prevStage": null,
+                    "nextStage": 2,
+                },
+                {
+                    "id": 2,
+                    "action": "Sort",
+                    "isStart": false,
+                    "prevStage": 1,
+                    "nextStage": 3,
+                },
+                {
+                    "id": 3,
+                    "action": "Extract",
+                    "isStart": false,
+                    "prevStage": 2,
+                    "nextStage": null,
+                },
+                {
+                    "id": 4,
+                    "action": "Export",
+                    "isStart": false,
+                    "prevStage": null,
+                    "nextStage": null,
+                }
+            ]
         };
     }
 
@@ -16,6 +46,7 @@ export class WorkflowsPage extends React.Component {
             <div className='App'>
                 <NavBar />
                 <div>
+                    <WorkflowsList workflows={this.state.workflows}/>
                 </div>
                 <div>
                     <Button
